ae06dd2abfe16deba7c650a24855d0b2bc43d6fe
sarthak2205 <nirmal.sarthak@gmail.com>
2023-09-20 12:43:49 +0530

    2nd commit

--- Client/src/pages/Reports/Generation_Report-1.tsx
+++ Client/src/pages/Reports/Generation_Report-1.tsx
@@ -36,16 +36,10 @@ const Generation_Report = () => {
     contact: string;
   }
 
-  const timeperiodHeaderMap: Record<string, string> = {
-    all: "Date",
-    week: "Week",
-    month: "Month",
-  };
-
   const [waste, setWaste] = useState([]);
   const [ hospitalID, setHospitalID] = useState("");
 
-  const [ timeduration, setTimeDuration ] = useState("all");
+  const [ timeduration, setTimeDuration ] = useState("All");
 
   const pageSize = 10;
   const [ currPage, setCurrPage ] = useState(1);
@@ -71,7 +65,7 @@ const Generation_Report = () => {
             timeduration: timeduration,
           }
         });
-        //console.log("Data is: ", res);
+        console.log("Data is: ", res);
         setWaste(res.data);
       } catch(error) {
         console.log("Error in Generation Report: ", error)
@@ -84,10 +78,51 @@ const Generation_Report = () => {
 
   //console.log("Hospital ID ", hospitalID)
   //console.log("Waste itme: ", waste)
-  //console.log("Time Duration: ", timeduration);
+  console.log("Time Duration: ", timeduration);
   // Group the processed data by bag color
+  const processAndGroupData = (data: BagData[]) => {
+    const groupedData: { [key: string]: BagData[] } = {};
+    data.forEach((bag: BagData) => {
+        const pickupDateTime = bag['Pick up DateTime'];
+        const pickupDate = pickupDateTime.split(' ')[0]; // Extract the date part
+        const [year, month, day] = pickupDate.split('-'); // Split the date into parts
+        const formattedDate = `${day}-${month}-${year}`; // Format it as DD-MM-YYYY
 
-  const currentBags = waste.slice(startIndex, endIndex);
+        if (!groupedData[formattedDate]) {
+            groupedData[formattedDate] = [];
+        }
+        groupedData[formattedDate].push(bag);
+    });
+
+    const todayDate = new Date().toLocaleDateString('en-GB', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+    });
+
+    const sortedKeys = Object.keys(groupedData).sort((a, b) => {
+      if (a === todayDate) {
+        return -1; // Today's date should come first
+      }
+      if (b === todayDate) {
+        return 1; // Today's date should come first
+      }
+      const dateA = new Date(a.split('-').reverse().join('-')).getTime();
+      const dateB = new Date(b.split('-').reverse().join('-')).getTime();
+      return dateB - dateA;
+    });
+
+    // Create a new object with sorted keys
+    const sortedGroupedData: { [key: string]: BagData[] } = {};
+    sortedKeys.forEach(key => {
+        sortedGroupedData[key] = groupedData[key];
+    });
+    return sortedGroupedData;
+  };
+
+  const groupedData = processAndGroupData(waste);
+
+  const currentBags = Object.entries(groupedData).slice(startIndex, endIndex);
   //console.log("Grouped Data is:", groupedData)
 
     return (
@@ -96,9 +131,10 @@ const Generation_Report = () => {
         <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
           <div className='w-1/3'>
             <Select label="Time Duration" value={timeduration} onChange={(e) => setTimeDuration(e)}>
-              <Option value="all">All</Option>
-              <Option value="week">Last Week</Option>
-              <Option value="month">Last Month</Option>
+              <Option value='All'>All</Option>
+              <Option value='Yesterday'>Yesterday</Option>
+              <Option value='Weekly'>Weekly</Option>
+              <Option value='Monthly'>Monthly</Option>
             </Select>
           </div>
           <div className="max-w-full overflow-x-auto">
@@ -119,7 +155,7 @@ const Generation_Report = () => {
                   </th>
       </tr>*/}
                   <tr className="bg-gray-2 text-left dark:bg-meta-4">
-                    {["Sl. No.", timeperiodHeaderMap[timeduration], "Red Bags", "Yellow Bags", "Blue Bags", "Sharps"].map((el) => (
+                    {["Sl. No.", "Date", "Red Bags", "Yellow Bags", "Blue Bags", "Sharps"].map((el) => (
                       <th
                       key={el} 
                       className="py-5 px-1 font-medium text-black dark:text-white xl:pl-11">
@@ -129,32 +165,23 @@ const Generation_Report = () => {
                   </tr>
               </thead>
               <tbody>
-                {currentBags.map((item, index) => (
+                {currentBags.map(([pickupDate, bags], index) => (
                   <tr key={index}>
                       <td className='py-5 px-1 xl:pl-10 text-black dark:text-white text-left'>{index + 1}</td>
                       <td className='py-5 px-1 xl:pl-10 text-black dark:text-white text-left'>
-                        
-                        {
-                            timeduration === "all" ? <Link to={`/Generation_Report/${item['Date']}`}>{item['Date']}</Link> :
-                            timeduration === "week" ? item['Week']:
-                            timeduration === "month" ? item['Month']:
-                            timeduration === "quaterly" ? item['Quarter']:
-                            timeduration === "half yearly" ? item['Half']:
-                            timeduration === "yearly" ? item['Year']:
-                            "NaN"
-                          }
+                        <Link to={`/Generation_Report/${pickupDate}`}>{pickupDate}</Link>
                       </td>
                       <td className='py-5 px-1 xl:pl-10 text-black dark:text-white text-left'>
-                        {item['Red_bags']}/{item['Red_weight']}
+                        {`${bags.find(bag => bag.bagColor === 'Red')?.['Number of Bags'] || '0'}/${bags.find(bag => bag.bagColor === 'Red')?.['Weight of Bags'] || '0'} kgs`}
                       </td>
                       <td className='py-5 px-1 xl:pl-10 text-black dark:text-white text-left'>
-                        {item['Yellow_bags']}/{item['Yellow_weight']}
+                        {`${bags.find(bag => bag.bagColor === 'Yellow')?.['Number of Bags'] || '0'}/${bags.find(bag => bag.bagColor === 'Yellow')?.['Weight of Bags'] || '0'} kgs`}
                       </td>
                       <td className='py-5 px-1 xl:pl-10 text-black dark:text-white text-left'>
-                        {item['Blue_bags']}/{item['Blue_weight']}
+                        {`${bags.find(bag => bag.bagColor === 'Blue')?.['Number of Bags'] || '0'}/${bags.find(bag => bag.bagColor === 'Blue')?.['Weight of Bags'] || '0'} kgs`}
                       </td>
                       <td className='py-5 px-1 xl:pl-10 text-black dark:text-white text-left'>
-                        {item['White_bags']}/{item['White_weight']}
+                        {`${bags.find(bag => bag.bagColor === 'White')?.['Number of Bags'] || '0'}/${bags.find(bag => bag.bagColor === 'White')?.['Weight of Bags'] || '0'} kgs`}
                       </td>
                   </tr>
                 ))}
@@ -163,9 +190,9 @@ const Generation_Report = () => {
             <div className='flex justify-center items-center relative'>
               <Pagination
                 color={'primary'}
-                count={waste.length / pageSize}
+                count={Math.ceil(Object.entries(groupedData).length / pageSize)}
                 page={currPage}
-                onChange={(event: any, page: any) => setCurrPage(page)}
+                onChange={(event, page) => setCurrPage(page)}
               />
               {/*<div className='absolute left-0'>
                 Showing {currentBags.length} of {Object.entries(groupedData).reduce((total, arr) => total + arr.length, 0)} Entries
